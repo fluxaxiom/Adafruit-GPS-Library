ef27467e2578920c6ab782f8df68b5621b6d3f50
fluxaxiom <frankappio@gmail.com>
2015-08-25 00:10:17 -0400

    fixing library

--- Adafruit_GPS.cpp
+++ Adafruit_GPS.cpp
@@ -5,6 +5,7 @@ Adafruit invests time and resources providing this open source code,
 please support Adafruit and open-source hardware by purchasing
 products from Adafruit!
 
+ 
 Written by Limor Fried/Ladyada for Adafruit Industries.
 BSD license, check license.txt for more information
 All text above must be included in any redistribution
